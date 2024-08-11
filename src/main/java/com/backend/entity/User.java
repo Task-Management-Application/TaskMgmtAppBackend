db5ca8eebe874db2000f75b8de0fcb835b25cbf7
shayantan <shayantansarkar987@gmail.com>
2024-08-11 05:19:55 -0800

    Added attributes to the user class

--- src/main/java/com/backend/entity/User.java
+++ src/main/java/com/backend/entity/User.java
@@ -1,6 +1,12 @@
 package com.backend.entity;
 
+import java.util.Date;
+
 public class User {
 	private Integer userId;
 	private String emailId;
+	private String password;
+	private String dob;
+	private Date createdAt;
+	private Date lastLoginAt;
 }
