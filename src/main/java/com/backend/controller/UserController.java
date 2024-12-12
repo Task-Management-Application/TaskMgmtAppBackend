ee195be4903ae906d9b5bd881f72ac43428b982a
shayantan <shayantansarkar987@gmail.com>
2024-12-12 09:53:45 -0800

    Supported profile pic upload and download

--- src/main/java/com/backend/controller/UserController.java
+++ src/main/java/com/backend/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.backend.controller;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +19,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.backend.dto.CommentDto;
 import com.backend.dto.OrganisationDto;
@@ -23,6 +31,7 @@ import com.backend.response.ApiResponse;
 import com.backend.service.OrganisationService;
 import com.backend.service.UserService;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 
 @RestController
@@ -107,4 +116,41 @@ public class UserController {
                 this.organisationService.updateAdmin(requestingUserId, requestedUserId, true, organisationId);
         return new ResponseEntity(HttpStatus.OK);
     }
+
+    // profile picture update
+    @PostMapping("/{userId}/profile-pic")
+    public ResponseEntity<String> uploadProfilePic(
+            @PathVariable Integer userId,
+            @RequestParam("file") MultipartFile file) {
+        try {
+            String fileName = userService.uploadProfilePic(userId, file);
+            return ResponseEntity.ok("Profile picture uploaded successfully "+fileName);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Failed to upload profile picture");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{userId}/profile-pic")
+    public ResponseEntity<String> downloadProfilePic(
+            @PathVariable Integer userId,
+            HttpServletResponse response
+    ) throws IOException {
+
+        try{
+            InputStream resource = userService.downloadprofilePic(userId);
+            response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+            StreamUtils.copy(resource,response.getOutputStream());
+            return new ResponseEntity<>("Showing the profile pic",HttpStatus.FOUND);
+        }
+        catch(FileNotFoundException e)
+        {
+            return new ResponseEntity<>("File not found for the user profile pic",HttpStatus.NOT_FOUND);
+        }
+        
+    }
+
 }
--- src/main/java/com/backend/service/UserService.java
+++ src/main/java/com/backend/service/UserService.java
@@ -1,8 +1,12 @@
 package com.backend.service;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.backend.dto.CommentDto;
 import com.backend.dto.OrganisationDto;
@@ -32,4 +36,8 @@ public interface UserService {
     List<UserDto> addUserToOrganisation(Integer requestingUserId, Integer requestedUserId, Integer orgId);
 
     List<OrganisationDto> getAllOrganisationsForuserId(Integer userId);
+
+    InputStream downloadprofilePic(Integer userId) throws FileNotFoundException;
+
+    String uploadProfilePic(Integer userId, MultipartFile file) throws IOException;
 }
